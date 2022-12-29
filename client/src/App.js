c9b438addfc26b6786acd1c6f83152a16be2dd6a
riya <riyasinghsirohi@gmail.com>
2022-12-29 15:30:20 +0530

    login pages

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import AuthentificationState from "./contexts/auth/Auth.state";
+import Routing from "./routes/Routing";
+import "./App.css";
+
+const App = () => {
+	return (
+		<AuthentificationState>
+			<Routing />
+		</AuthentificationState>
+	);
+};
+
+export default App;
